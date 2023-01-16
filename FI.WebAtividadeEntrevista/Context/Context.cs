8be6548e463ad3ba59160a489808210365b581c0
Gustavo Enricco Goncalves Alexandre <gustavoalexandre@employer.com.br>
2023-01-16 09:09:19 -0300

    Adicionar arquivos de projeto.

---
+++ FI.WebAtividadeEntrevista/Context/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+using WebAtividadeEntrevista.Models;
+
+namespace WebAtividadeEntrevista.Context
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options):base(options)
+        
+            {
+
+            }
+        public DbSet<ClienteModel> ClienteModel { get; set; }
+    }
+}
